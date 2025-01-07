89df46a22eedb59112798aef3512e7576c001d43
Stephan Mugisho <stephanemugisho24@gmail.com>
2025-01-07 17:26:02 +0200

    connexion avec le backend et creation de la page d'inscription

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
+import cors from "cors"
 
 import {connectDB} from "./lib/db.js"
 
@@ -11,6 +12,12 @@ dotenv.config()
 const app = express();
 
 const PORT= process.env.PORT
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+    
+}))
 app.use(express.json());
 app.use(cookieParser());
 
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,9 +1,136 @@
+import { useState } from "react"
+import { useAuthStore } from "../store/useAuthStore";
+import { MessageSquare,User, Mail, Lock, EyeOff, Eye, Loader2 } from "lucide-react";
+import { Link } from "react-router-dom";
+import AuthImagePattern from "../components/AuthImagePattern";
+import toast from "react-hot-toast";
 
 
 const SignUpPage = () => {
-  return (
-    <div>SignUpPage</div>
-  )
+  const [showPassword, setShowPassword]=useState(false)
+  const [formData, setFormData]=useState({
+    fullName:"",
+    email:"",
+    password:"",
+  });
+
+  const {signup, isSigningUp}=useAuthStore();
+
+  const validateForm=()=>{}
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+  }
+
+  return <div className="min-h-screen grid lg: grid-cols-2">
+    {/* left side*/ }
+    <div className="flex flex-col justify-center items-center p-6 sm:p-12">
+      <div className="w-full max-w-md space-y-8">
+        {/* logo*/}
+        <div className="text-center mb-8">
+          <div className="flex flex-col items-center gap-2 group">
+            <div
+              className="size-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-primary/20 transition-colors"
+            >
+              <MessageSquare className="size-6 text-primary"/>
+
+            </div>
+            <h1 className="text-2xl font-bold mt-2">Creer votre compte</h1>
+            <p className="text-base-content/60">Commencer avec um compte gratuit</p>
+          </div>
+        </div>
+
+        <form onSubmit={handleSubmit} className="space-y-6">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text font-medium">Noms complet</span>
+            </label>
+            <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <User className="size-5 text-base-content/40" />
+                </div>
+                <input
+                  type="text"
+                  className={`input input-bordered w-full pl-10`}
+                  placeholder="John Doe"
+                  value={formData.fullName}
+                  onChange={(e) => setFormData({ ...formData, fullName: e.target.value })}
+                />
+              </div>
+          </div>
+          <div className="form-control">
+              <label className="label">
+                <span className="label-text font-medium">Email</span>
+              </label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <Mail className="size-5 text-base-content/40" />
+                </div>
+                <input
+                  type="email"
+                  className={`input input-bordered w-full pl-10`}
+                  placeholder="you@example.com"
+                  value={formData.email}
+                  onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                />
+              </div>
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-medium">Mot de passe</span>
+              </label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <Lock className="size-5 text-base-content/40" />
+                </div>
+                <input
+                  type={showPassword ? "text" : "password"}
+                  className={`input input-bordered w-full pl-10`}
+                  placeholder="••••••••"
+                  value={formData.password}
+                  onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+                />
+                <button
+                  type="button"
+                  className="absolute inset-y-0 right-0 pr-3 flex items-center"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? (
+                    <EyeOff className="size-5 text-base-content/40" />
+                  ) : (
+                    <Eye className="size-5 text-base-content/40" />
+                  )}
+                </button>
+              </div>
+            </div>
+            <button type="submit" className="btn btn-primary w-full" disabled={isSigningUp}>
+              {isSigningUp ? (
+                <>
+                  <Loader2 className="size-5 animate-spin" />
+                  Loading...
+                </>
+              ) : (
+                "Creer votre Compte"
+              )}
+            </button>
+        </form>
+        <div className="text-center">
+            <p className="text-base-content/60">
+              Vous avez deja un compte?{" "}
+              <Link to="/login" className="link link-primary">
+                Se Connecter
+              </Link>
+            </p>
+        </div>
+      </div>
+    </div>
+     {/* right side */}
+
+     <AuthImagePattern
+        title="Rejoigner notre communaute"
+        subtitle="Connect with friends, share moments, and stay in touch with your loved ones."
+      />
+  </div>
+  
 }
 
-export default SignUpPage
+export default SignUpPage;
