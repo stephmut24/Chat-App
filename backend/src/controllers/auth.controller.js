d17fa4eb8b9736dbf3d006fe8d434d733a474fbe
Stephan Mugisho <stephanemugisho24@gmail.com>
2025-01-07 12:22:15 +0200

    j'ai fini de cree la partie la plus importante du back, je passe maintenant au frontend avec reac

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -96,7 +96,7 @@ export const updateProfile=async(req, res)=>{
         const updatedUser= await User.findByIdAndUpdate(userId,{profilePic:uploadResponse.secure_url}, {new:true})
         res.status(200).json(updatedUser);
     } catch (error) {
-        console.log("error in update profile:", error);
+        console.error("error in update profile:", error);
         res.status(500).json({message: "Internal server error"});
         
     }
@@ -107,7 +107,7 @@ export const checkAuth=(req, res)=>{
     try {
         res.status(200).json(req.user)
     } catch (error) {
-        console.log("Error in checkAuth controller", error.message);
+        console.error("Error in checkAuth controller", error.message);
         res.status(500).json({message:"internal server error"});
     }
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,21 @@
 import express from "express";
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+
 import {connectDB} from "./lib/db.js"
 
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
 
 dotenv.config()
 const app = express();
 
 const PORT= process.env.PORT
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 
 app.listen(PORT, () =>{
