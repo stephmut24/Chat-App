08fc8340f5418529ba66ff9e0b0bc6dd436e9dac
Stephan Mugisho <stephanemugisho24@gmail.com>
2025-01-07 11:22:45 +0200

    Deuxieme commit

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,9 +1,18 @@
 import express from "express"
-import { signup } from "../controllers/auth.controller.js";
+import { signup, login, logout,updateProfile, checkAuth} from "../controllers/auth.controller.js";
+import {protectRoute} from "../middleware/auth.middleware.js";
+
 
 const router = express.Router();
 
 router.post("/signup", signup);
-//router.post("/login", login);
-//router.post("/logout", logout );
+router.post("/login", login);
+router.post("/logout", logout );
+
+router.put("/update-profile",protectRoute,updateProfile)
+
+router.get("/check", protectRoute,checkAuth)
+
+
+
 export default router;
