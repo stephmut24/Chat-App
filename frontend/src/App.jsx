d5dd82c7335cae67e7ad4bf71671d4be5256338f
Stephan Mugisho <stephanemugisho24@gmail.com>
2025-01-11 02:31:09 +0200

    ajout de Toaster pour les notifications

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,8 @@ import  ProfilePage from "./pages/ProfilePage";
 import { useAuthStore } from "./store/useAuthStore";
 import { useEffect } from "react";
 import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
+
 
 
 const App = () => {
